f3b5403ac4b23bf9bf3c604592cd49e90ee9b782
Son-Draken <Kovis12@seznam.cz>
2023-02-08 15:12:02 +0100

    pridani datasetu

---
+++ DataSet2/DataSet2/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;        
+
+namespace DataSet2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //connection string varinata 1
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog = KopieDatabaze; Integrated Security = True";
+
+            //conection string varinata 2
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = @"(localdb)\MSSQLLocalDB";  //umístění databáze
+            csb.InitialCatalog = "KopieDatabaze";   //název databaze
+            csb.IntegratedSecurity = true;
+            string connectionString2 = csb.ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString2);
+            
+            // nutno comand  pro každý table zvlášť , když snimi chci dále pracovat
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM  Category";
+            SqlDataAdapter adapterCategory = new SqlDataAdapter();
+            adapterCategory.SelectCommand = command;
+
+            SqlCommand command2 = new SqlCommand();
+            command2.Connection = connection;
+            command2.CommandText = "SELECT * FROM Word";
+            SqlDataAdapter adapterWord = new SqlDataAdapter();
+            adapterWord.SelectCommand = command2;
+            
+
+            //nastaveni Datasetu pro další zpracování dat
+
+            DataSet ds = new DataSet();
+            adapterCategory.Fill(ds, "Kategorie");
+            adapterWord.Fill(ds, "Slovicka");
+            
+            /*
+            foreach (DataRow row in ds.Tables["Slovicka"].Select("CategoryId = 2", "Czech"))
+                {
+                Console.WriteLine(row["Czech"].ToString().PadRight(20));
+                Console.WriteLine(row["Difficulty"]);
+                }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Další příklad");
+            Console.WriteLine(" ");
+            */
+          /*
+            DataRelation relation = new DataRelation("relaceKategorieSlovicka",
+                ds.Tables["Kategorie"].Columns["Id"],
+                ds.Tables["Slovicka"].Columns["CategoryId"]);
+            ds.Relations.Add(relation);
+
+            foreach (DataRow rowCategory in ds.Tables["Kategorie"].Rows)
+            {
+                Console.WriteLine(rowCategory["Title"]);
+                foreach (DataRow rowWord in rowCategory.GetChildRows("relaceKategorieSlovicka"))
+                {
+                    Console.WriteLine("   {0}", rowWord["Czech"]);
+                }
+            }
+          */
+            Console.WriteLine(" ");
+            Console.WriteLine("Další příklad");
+            Console.WriteLine(" ");
+
+
+
+            //přidání nové kategorie "Rodina"
+
+            
+            DataRow newCategory = ds.Tables["Kategorie"].NewRow();
+            newCategory["Title"] = "Slon";
+            ds.Tables["Kategorie"].Rows.Add(newCategory);
+
+
+            foreach (DataRow rowCategory in ds.Tables["Kategorie"].Rows)
+            {
+                Console.WriteLine(rowCategory["Title"]);
+            }
+            
+
+            //odstranění kategorie např. "Rodina"
+            /*
+            DataRow[] deleteRow = ds.Tables["Kategorie"].Select("Title='Rodina'");
+            if (deleteRow.Length !=1)
+            {
+                Console.WriteLine("Zaznam na smazán nenalezen");
+            }
+            else
+            {
+                DataRow row = deleteRow[0];
+                row.Delete();
+                Console.WriteLine("Kategorie smazána");
+            }
+            */
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Další příklad");
+            Console.WriteLine(" ");
+
+
+            /* editace záznamu
+
+            DataRow[] searchRows = ds.Tables["Slovicka"].Select("Czech='míč'");
+            if (searchRows.Length != 1)
+            {
+                Console.WriteLine("Chyba nenalezený požadovaná záznam.");
+            }
+            else
+            {
+                DataRow row = searchRows[0];
+                row.BeginEdit();
+                row["Difficulty"] = 2;
+                row.EndEdit();
+                Console.WriteLine("změna provedena");
+            }
+            
+
+            //doplnění hodnoty difficulty u slovíček bez hodnot s NULL
+            /*
+            DataRow[] changeRow = ds.Tables["Slovicka"].Select("Difficulty IS NULL");
+            foreach (DataRow rowDif in changeRow)
+            {
+                DataRow row = rowDif;
+                row.BeginEdit();
+                row["Difficulty"] = 1;
+                row.EndEdit();
+
+
+            }
+            Console.WriteLine("Provedeno vše");
+            */
+
+
+            //uložení změn, aktualizace databaze
+
+
+            SqlCommandBuilder cbSlovicka = new SqlCommandBuilder(adapterWord);
+            SqlCommandBuilder cbKategorie = new SqlCommandBuilder(adapterCategory);
+            adapterCategory.Update(ds.Tables["Kategorie"]);
+            adapterWord.Update(ds.Tables["Slovicka"]);
+    
+            connection.Close();
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
---
+++ DataSetAdapter/DataSetAdapter/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace DataSetAdapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //connection string varinata 1
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog = KopieDatabaze; Integrated Security = True";
+
+            //conection string varinata 2
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = @"(localdb)\MSSQLLocalDB";  //umístění databáze
+            csb.InitialCatalog = "KopieDatabaze";   //název databaze
+            csb.IntegratedSecurity = true;
+            string connectionString2 = csb.ConnectionString;
+
+
+
+
+            /*using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Word";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                using (DataSet result = new DataSet())
+                {
+                    adapter.Fill(result);
+
+
+                    foreach (DataRow row in result.Tables[0].Rows)
+                    {
+                        Console.WriteLine("Id: "+ row[0] + ", česky: " + row[1] + ", anglicky: " + row[2]);
+                    }
+                }
+                connection.Close();
+                Console.ReadKey();
+            */
+            string repeat = "yes";
+            while (repeat == "yes")
+            { 
+                using (SqlConnection connection = new SqlConnection(connectionString2))
+            
+              
+                {
+                    connection.Open();
+
+                    Console.WriteLine("Zadej slovíčko na překlad:");
+                    string word = Console.ReadLine().ToLower();
+                    string query = "SELECT * FROM Word WHERE English=@word OR Czech=@word";
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                    using (DataSet result = new DataSet())
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("@word", word);
+                        adapter.Fill(result);
+
+                        Console.WriteLine("Zadal jste slovo: "+ word);
+
+                        if (result.Tables[0].Rows.Count != 0)
+                        {
+                            if (result.Tables[0].Rows[0]["English"].Equals(word))
+                            Console.WriteLine("Překlad je: {0}", result.Tables[0].Rows[0]["Czech"]);
+                            else
+                            Console.WriteLine("Překlad je: {0}", result.Tables[0].Rows[0]["English"]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Záznam nenalezen");
+                        }
+                    }
+
+                    Console.WriteLine("Chcete pokračovat: yes/no ?");
+                    Console.Write("Zadejte vaší volbu: ");
+                    repeat = Console.ReadLine().ToLower();
+                    
+                }
+             
+            }
+            Console.WriteLine("Děkuji za použití překladače...sbohem");
+            Console.ReadKey();
+        }
+    }
+}
